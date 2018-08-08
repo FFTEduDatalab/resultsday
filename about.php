1c830eeb36142f6c906e0e245bb87d3040937bab
philipnye <contact@philipnye.co.uk>
2018-08-08 14:43:45 +0100

    Add first draft about page text

--- about.php
+++ about.php
@@ -14,7 +14,77 @@
 		</div>
 		<div class="row">
 			<div class="col s12">
-				<p>Created by FFT Education Datalab with the support of the Nuffield Foundation.</p>
+				<h3>Background</h3>
+				<p>This microsite provides analysis of national GCSE and A-Level results in England, Wales and Northern Ireland from 2014 to 2018. The data has been sourced from the Joint Council for Qualifications. The <a href="#nuffield">Nuffield Foundation</a> has provided funding for the development of the site.</p>
+
+				<p>There has been substantial policy change at both Key Stage 4 and Key Stage 5 during this time. All three nations have reviewed their curriculum and adopted different approaches in response. These changes, summarised below, affect comparisons between the three nations and comparisons over time within each nation.</p>
+
+				<h3>The data</h3>
+				<p>The data for the microsite is sourced from the Joint Council for Qualifications (JCQ), a membership organisation for the eight largest awarding organisations in the UK.</p>
+
+				<p>Each year, JCQ collates the aggregate results of A-Levels and GCSEs delivered by its members and publishes them on its website. UK-wide results are published for male and female students, and all students.</p>
+
+				<p>The published data is provisional. Results may subsequently change due to appeals and remarks. Comparisons to previous years are similarly based on provisional results.</p>
+
+				<p>Sources are given below:</p>
+
+				<em>Exam entries and grades awarded, JCQ</em>
+				<ul class='browser-default'>
+					<li><a href='https://www.jcq.org.uk/examination-results/gcses'>GCSE</a></li>
+					<li><a href='https://www.jcq.org.uk/examination-results/a-levels'>A-Level</a></li>
+				</ul>
+
+				<em>Dates of reform</em>
+				<ul class='browser-default'>
+					<li>England, Ofqual</li>
+					<ul class='browser-default'>
+						<li><a href='https://www.gov.uk/government/publications/get-the-facts-gcse-and-a-level-reform/get-the-facts-gcse-reform'>GCSE</a></li>
+						<li><a href='https://www.gov.uk/government/publications/get-the-facts-gcse-and-a-level-reform/get-the-facts-as-and-a-level-reform'>A-Level</a></li>
+					</ul>
+					<li>Wales, Qualifications Wales</li>
+					<ul class='browser-default'>
+						<li><a href='http://qualificationswales.org/english/qualifications/gcses-and-a-levels/gq-reform/what-subjects-are-available/'>GCSE, A-Level</a></li>
+					</ul>
+					<li>Northern Ireland, Council for the Curriculum, Examinations and Assessment</li>
+					<ul class='browser-default'>
+						<li><a href='http://ccea.org.uk/regulation/qualsni/changes_gcses'>GCSE</a></li>
+						<li><a href='http://ccea.org.uk/regulation/qualsni/changes_levels'>A-Level</a></li>
+					</ul>
+				</ul>
+
+				<p>Date of reform relates to the date when most students in a given home nation will be entered reformed versions of the qualifications. Some students may enter qualifications offered by an exam board in a home nation other than the one in which they live.</p>
+
+				<em>EBacc subjects, Department for Education</em>
+				<ul class='browser-default'>
+					<li><a href='https://www.gov.uk/government/publications/english-baccalaureate-eligible-qualifications'>EBacc eligible qualifications</a></li>
+				</ul>
+
+				<em>Progress 8 subjects, Department for Education</em>
+				<ul class='browser-default'>
+					<li><a href='https://www.gov.uk/government/publications/progress-8-school-performance-measure'>Double-counted subjects</a></li>
+				</ul>
+
+				<em>Facilitating subjects, Russell Group</em>
+				<ul class='browser-default'>
+					<li><a href='https://russellgroup.ac.uk/for-students/school-and-college-in-the-uk/subject-choices-at-school-and-college/'>Eligible subjects</a></li>
+				</ul>
+
+				<h3>Credits</h3>
+				<ul class='browser-default'>
+					<li>Funding: Nuffield Foundation</li>
+					<li>Site design: Geoff Solomon, Philip Nye</li>
+					<li>Data analysis: Philip Nye, Dave Thomson</li>
+					<li>User testing: XXX</li>
+				</ul>
+
+				<h3 id='nuffield'>The Nuffield Foundation</h3>
+				<em><p>The Nuffield Foundation funds research, analysis, and student programmes that advance educational opportunity and social well-being across the United Kingdom.</p>
+
+				<p>We want to improve people’s lives, and their ability to participate in society, by understanding the social and economic factors that affect their chances in life. The research we fund aims to improve the design and operation of social policy, particularly in Education, Welfare, and Justice.</p>
+
+				<p>Our student programmes – Nuffield Research Placements and Q-Step – provide opportunities for individual students, particularly those from disadvantaged backgrounds, to develop their skills and confidence in quantitative and scientific methods.</p>
+
+				<p>We are an independent charitable trust established in 1943 by William Morris, Lord Nuffield, the founder of Morris Motors. <a href='https://www.nuffieldfoundation.org'>www.nuffieldfoundation.org</a> | <a href='www.twitter.com/NuffieldFound'>@NuffieldFound</a></p></em>
 			</div>
 		</div>
 	</div>
