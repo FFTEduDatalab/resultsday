6e991a3c7c8f8360852ca70fc0f294b6f30ce5fe
philipnye <contact@philipnye.co.uk>
2018-08-14 17:12:21 +0100

    Header, footer fixes

--- inc/footer.php
+++ inc/footer.php
@@ -33,7 +33,6 @@
 		</div>
 	</div>
 	</footer>
-	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 	<script src="/js/materialize.min.js"></script>
 	<script src="/js/init-0.1.0.js"></script>
 	<script>
--- inc/header.php
+++ inc/header.php
@@ -7,7 +7,7 @@
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link href="/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 	<link href="/css/style-0.1.0.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-	<link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+	<link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://code.highcharts.com/highcharts.js"></script>
 	<script src="https://code.highcharts.com/modules/series-label.js"></script>
@@ -17,9 +17,8 @@
 	<script src="/js/theme-0.1.1.js"></script>
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})
+		(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 		ga('create', 'UA-59588201-1', 'auto');
 		ga('send', 'pageview');
 	</script>
