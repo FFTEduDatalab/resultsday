e75e938303786badacab6bc48c2b0c38bb90da3e
philipnye <contact@philipnye.co.uk>
2018-08-14 12:20:28 +0100

    Fix chart responsive height issue (fix chart height)

---
+++ js/theme-0.1.1.js
@@ -0,0 +1,69 @@
+Highcharts.theme = {
+	chart: {
+		style: {
+			fontFamily: 'Arial'
+		},
+		height:500
+	},
+    colors: ['#2daae1', '#96c11f', '#535353'],
+    lang: {
+      thousandsSep: ',',
+      numericSymbols: null,
+	  contextButtonTitle: 'Menu'
+    },
+    plotOptions: {
+        series: {
+            marker: {
+                symbol: 'circle'
+            }
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    xAxis: {
+        min: 2014,
+        max: 2018,
+        tickInterval: 1
+    },
+    yAxis: {
+        title: {
+          enabled: false
+        },
+        min: 0,
+		allowDecimals: false,
+        labels: {   // Use thousands separator for four-digit numbers too
+        	formatter: function () {
+            	var label = this.axis.defaultLabelFormatter.call(this);
+                if (/^[0-9]{4}$/.test(label)) {
+                	return Highcharts.numberFormat(this.value, 0);
+                }
+                return label;
+            }
+        }
+    },
+    responsive: {
+        rules: [{
+            condition: {
+				maxWidth: 400,
+            },
+            chartOptions: {
+				credits:{
+					href: 'http://results.ffteducationdatalab.org.uk',
+					text: 'Source: http://results.ffteducationdatalab.org.uk'
+				}
+            }
+        }]
+    },
+	exporting: {
+          buttons: {
+				contextButton: {
+					menuItems: ['printChart', 'separator', 'downloadPNG', 'downloadJPEG', 'separator', 'downloadCSV']
+				}
+          }
+      },
+	credits:{
+		href: 'http://results.ffteducationdatalab.org.uk',
+		text: 'Source: FFT Education Datalab analysis of JCQ data. Funded by the Nuffield Foundation.'
+	}
+};
