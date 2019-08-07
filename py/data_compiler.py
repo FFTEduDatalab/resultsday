8ce11517dc058324a61b1b9f0664a58333382968
philipnye <contact@philipnye.co.uk>
2019-08-07 15:38:41 +0100

    Reduce unnecessary rewriting of data

--- py/data_compiler.py
+++ py/data_compiler.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
+# NB: This script is somewhere between rebuilding the data from scratch each time, and only adding new years. For a given level, if there are news years then all data for that level is rebuilt from scratch, but data for other levels isn't
 
 import os
 import re
 import json
 from xlrd import open_workbook
 from collections import OrderedDict
+
+mode='normal'		# used to control whether only new years are added to the data, or if the data is written from scratch
+# mode='testing'
 
 genders = ['Male','Female','All students']
 
@@ -30,12 +34,29 @@ levels=[
 	}
 ]
 
+def check_existing_coverage():
+	global latest_output_year
+	latest_output_year=None
+	for entry in entries_data:
+		if latest_output_year is None or entry['data'][0][0]>latest_output_year:
+			latest_output_year=entry['data'][-1][0]
+	return latest_output_year
+
 for level in levels:
 	os.chdir(os.path.dirname( __file__ ))
 	os.chdir('..')
 	os.chdir(level['output'])
-	with open(level['name'].lower()+'-subjects.json') as subjects_file:
-		subjects_data = json.load(subjects_file)
+	subjects_filename=level['name'].lower()+'-subjects.json'
+	entries_filename=level['name'].lower()+'-entries.json'
+	grades_filename=level['name'].lower()+'-grades.json'
+	entries_data=[]
+	with open(subjects_filename) as subjects_file:
+		subjects_data=json.load(subjects_file)
+	if os.path.isfile(entries_filename):
+		with open(entries_filename) as entries_file:
+			entries_data=json.load(entries_file)
+	if mode=='normal':
+		check_existing_coverage()
 	os.chdir(os.path.dirname( __file__ ))
 	os.chdir('..')
 	os.chdir(level['source'])
@@ -45,50 +66,60 @@ for level in levels:
 	entries_list=[]
 	grades_list=[]
 	rows=[]
-
+	needs_saving=0
+	latest_source_year = None
 	for source_file in os.listdir('.'):
 		if source_file.endswith('.xls') or source_file.endswith('.xlsx'):
 			filename=source_file.split('.')[0]
 			filename_split=filename.split('_')
 			year=int(filename_split[1])
-			if level['name']!='GCSE' or (level['name']=='GCSE' and ((year<2017 and filename_split[3]=='ag') or (year>=2017 and filename_split[3]=='keygrades'))):		# need to only pick up one file, or else will have e.g. two 2017 entries figures where a subject features in a key grades file and the A*-G grades file
-				scope=filename_split[2].upper()
-				print filename
-				try:
-					rb=open_workbook(source_file)
-					rbws=rb.sheet_by_index(0)
-					for rbrow in range(11,rbws.nrows):			# ditching 10 header rows
-						row=OrderedDict.fromkeys(row, None)
-						if rbws.cell(rbrow,0).value!='' and rbws.cell(rbrow,0).value[0]!='(':		# ditches blank rows and table notes
-							subject_name = re.match('[^0-9]+[^()0-9]+[)]*',rbws.cell(rbrow,0).value).group(0).strip()
-							alias=''
-							for subject in subjects_data:
-								if any(subject_name.lower()==subj.lower() for subj in subject['subject_names'])==True:
-									alias=subject['alias']
-									break
-						if alias!='':
-							if rbws.cell(rbrow,1).value in ['Male', 'Female', 'Male & Female']:		# ditches previous year's results
-								row['alias']=alias
-								row['scope']=scope
-								row['year']=year
-								if rbws.cell(rbrow,1).value=='Male & Female':
-									row['gender']='All students'
-								else:
-									row['gender']=rbws.cell(rbrow,1).value
-								row['entries']=int(rbws.cell(rbrow,2).value)
-								if level['name']!='GCSE' or (level['name']=='GCSE' and filename_split[3]=='keygrades'):		# A-Level, AS-Level and GCSE key grades files
-									rbcol=4
-									for grade in level['grades']:
-										row[grade]=round(rbws.cell(rbrow,rbcol).value,1)
-										rbcol+=1
-								elif level['name']=='GCSE' and filename_split[3]=='ag':		# GCSE all grades files
-									rbcols=[5,7,11,12]		# columns where A, C, G, U values are held
-									for grade, rbcol in zip(level['grades'], rbcols):
-										row[grade]=round(rbws.cell(rbrow,rbcol).value,1)
-								rows.append(row)
-				except Exception as ex:
-					print ex
-
+		if latest_source_year is None or year>latest_source_year:
+			latest_source_year=year
+	for source_file in os.listdir('.'):
+		if source_file.endswith('.xls') or source_file.endswith('.xlsx'):
+			filename=source_file.split('.')[0]
+			filename_split=filename.split('_')
+			year=int(filename_split[1])
+			if mode=='normal' and latest_source_year<=latest_output_year:
+				print filename + ' skipped'
+				continue		# loop continued rather than broken, so we still get this _skipped_ message
+			needs_saving=1
+			scope=filename_split[2].upper()
+			print filename + ' added'
+			try:
+				rb=open_workbook(source_file)
+				rbws=rb.sheet_by_index(0)
+				for rbrow in range(11,rbws.nrows):			# ditching 10 header rows
+					row=OrderedDict.fromkeys(row, None)
+					if rbws.cell(rbrow,0).value!='' and rbws.cell(rbrow,0).value[0]!='(':		# ditches blank rows and table notes
+						subject_name = re.match('[^0-9]+[^()0-9]+[)]*',rbws.cell(rbrow,0).value).group(0).strip()
+						alias=''
+						for subject in subjects_data:
+							if any(subject_name.lower()==subj.lower() for subj in subject['subject_names'])==True:
+								alias=subject['alias']
+								break
+					if alias!='':
+						if rbws.cell(rbrow,1).value in ['Male', 'Female', 'Male & Female']:		# ditches previous year's results
+							row['alias']=alias
+							row['scope']=scope
+							row['year']=year
+							if rbws.cell(rbrow,1).value=='Male & Female':
+								row['gender']='All students'
+							else:
+								row['gender']=rbws.cell(rbrow,1).value
+							row['entries']=int(rbws.cell(rbrow,2).value)
+							if level['name']!='GCSE' or (level['name']=='GCSE' and filename_split[3]=='keygrades'):		# A-Level, AS-Level and GCSE key grades files
+								rbcol=4
+								for grade in level['grades']:
+									row[grade]=round(rbws.cell(rbrow,rbcol).value,1)
+									rbcol+=1
+							elif level['name']=='GCSE' and filename_split[3]=='ag':		# GCSE all grades files
+								rbcols=[5,7,11,12]		# columns where A, C, G, U values are held
+								for grade, rbcol in zip(level['grades'], rbcols):
+									row[grade]=round(rbws.cell(rbrow,rbcol).value,1)
+							rows.append(row)
+			except Exception as ex:
+				print ex
 	for gender in genders:
 		for row in rows:
 			if row['gender']==gender:
@@ -107,7 +138,6 @@ for level in levels:
 					entries['data']=[]
 					entries['data'].append(data_item)
 					entries_list.append(entries)
-
 	for grade in level['grades']:
 		for row in rows:
 			if any(grades['name']==grade and grades['alias']==row['alias'] and grades['scope']==row['scope'] and grades['gender']==row['gender'] for grades in grades_list)==True:
@@ -126,24 +156,16 @@ for level in levels:
 				grades['data']=[]
 				grades['data'].append(data_item)
 				grades_list.append(grades)
-
 	os.chdir(os.path.dirname( __file__ ))
 	os.chdir('..')
 	os.chdir(level['output'])
 
-	grades_filename=level['name'].lower()+'-grades.json'
-	entries_filename=level['name'].lower()+'-entries.json'
-
-	# Minified
-	with open(grades_filename, 'w') as outfile:
-		json.dump(grades_list, outfile, separators=(',', ':'))
-
-	with open(entries_filename, 'w') as outfile:
-		json.dump(entries_list, outfile, separators=(',', ':'))
-
-	# If pretty printing is required
-	# with open(grades_filename, 'w') as outfile:
-	# 	json.dump(grades_list, outfile, indent=4, separators=(',', ': '))
-	#
-	# with open(entries_filename, 'w') as outfile:
-	# 	json.dump(entries_list, outfile, indent=4, separators=(',', ': '))
+	if needs_saving==1:
+		with open(entries_filename, 'w') as entries_file:		# minified
+			json.dump(entries_list, entries_file, separators=(',', ':'))
+		with open(grades_filename, 'w') as grades_file:
+			json.dump(grades_list, grades_file, separators=(',', ':'))
+		# with open(entries_filename, 'w') as entries_file:		# prettified
+		# 	json.dump(entries_list, outfile, indent=4, separators=(',', ': '))
+		# with open(grades_filename, 'w') as grades_file:
+		# 	json.dump(grades_list, outfile, indent=4, separators=(',', ': '))
