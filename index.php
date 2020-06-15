48cf46d3540484a64e78b8f36df216b8411b49d8
philipnye <contact@philipnye.co.uk>
2020-06-15 12:26:36 +0100

    Update feed to display 2020 results blogposts

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 		<div class="row">		<!-- feed item title: <h4> tag with class feed-item-title; feed item excerpt: <p> tag with class feed-item-desc -->
 			<div class="col s12">
 				<h2>Blogposts</h2>
-				<script src="//rss.bloople.net/?url=https%3A%2F%2Fffteducationdatalab.org.uk%2Ftag%2Fresults-2019%2Ffeed&showtitle=false&type=js"></script>
+				<script src="//rss.bloople.net/?url=https%3A%2F%2Fffteducationdatalab.org.uk%2Ftag%2Fresults-2020%2Ffeed&showtitle=false&type=js"></script>
 				<!-- <div id="feed-holding-message">
 					<p class="feed-item-desc">Posts analysing this year's results will appear here once they are published</p>
 					<p class="feed-item-desc">In the meantime, explore this site to see the trends in <a href="a-level.php?v=20190822.2.2">A-Level</a>, <a href="as-level.php?v=20190822.2.2">AS-Level</a> and <a href="gcse.php?v=20190822.2.2">GCSE</a> entries and grades up to 2018</p>
