064bf546b8f33b3953b808f63ba3acca19d8581e
philipnye <contact@philipnye.co.uk>
2020-08-12 14:26:24 +0100

    - Add % change calc to entries script - Delete EBacc entries/grades script

--- py/extract_generator.py
+++ py/extract_generator.py
@@ -32,225 +32,168 @@ levels=[
 nations=['UK','EN','WA','NI']
 ages=['15','16','17']
 
-# # Listing EBacc subjects
-# target='GCSE'
-# for level in levels:
-# 	if level['name']==target:
-# 		os.chdir(os.path.dirname( __file__ ))
-# 		os.chdir('..')
-# 		os.chdir(level['output'])
-# 		row_list=[]
-# 		with open(level['name'].lower()+'-subjects.json') as subjects_file:
-# 			subjects_data = json.load(subjects_file)
-# 		for subject in subjects_data:
-# 			if subject['flags']['ebacc']==True:
-# 				print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']))
-#
-# # Listing discontinued subjects
-# target='GCSE'
-# for level in levels:
-# 	if level['name']==target:
-# 		os.chdir(os.path.dirname( __file__ ))
-# 		os.chdir('..')
-# 		os.chdir(level['output'])
-# 		row_list=[]
-# 		with open(level['name'].lower()+'-subjects.json') as subjects_file:
-# 			subjects_data = json.load(subjects_file)
-# 		for subject in subjects_data:
-# 			if 'Discontinued' in str(subject['reform_year']['EN']):
-# 				print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']), subject['reform_year']['EN'])
+# Listing EBacc subjects
+target='GCSE'
+for level in levels:
+	if level['name']==target:
+		os.chdir(os.path.dirname( __file__ ))
+		os.chdir('..')
+		os.chdir(level['output'])
+		row_list=[]
+		with open(level['name'].lower()+'-subjects.json') as subjects_file:
+			subjects_data = json.load(subjects_file)
+		for subject in subjects_data:
+			if subject['flags']['ebacc']==True:
+				print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']))
 
-# # A-Level/AS-Level/GCSE entries, year comparison
-# target='GCSE'
-# scope='EN'
-# name='All students'
-# years=[2018, 2019]
-#
-# for level in levels:
-# 	if level['name']==target:
-# 		os.chdir(os.path.dirname( __file__ ))
-# 		os.chdir('..')
-# 		os.chdir(level['output'])
-# 		with open(level['name'].lower()+'-subjects.json') as subjects_file:
-# 			subjects_data = json.load(subjects_file)
-# 		with open(level['name'].lower()+'-entries.json') as entries_file:
-# 			entries_data = json.load(entries_file)
-# 		output_dict = [x for x in entries_data if x['name'] == name and x['scope']==scope]
-# 		for item in output_dict:
-# 			for subject in subjects_data:
-# 				if item['alias']==subject['alias']:
-# 					string=''
-# 					for year in years:
-# 						if item['data'][0][0]>year:		# add a blank value where year doesn't exist
-# 							string=string + ' '
-# 						for datum in item['data']:
-# 							if datum[0]==year:
-# 								if string=='':
-# 									string=str(datum[1])
-# 								else:
-# 									string=string + ', ' + str(datum[1])
-# 					try:
-# 						if int(string.rsplit(' ',1)[1])>100 :		# ditch small-entry subjects
-# 							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string)
-# 					except IndexError:
-# 						if int(string)>100:		# ditch small-entry subjects
-# 							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string)
-#
-# # A-Level/AS-Level/GCSE grades, year comparison
-# target='GCSE'
-# scope='EN16'
-# gender='All students'
-# grade='7/A or above'
-# # exclusions=['LEIS', 'AOTH', 'HSOC', 'HOME', 'MANU', 'HOSP', 'BUSC' ,'WELS', 'WELF', 'IRIS', 'PERF'] discontinued
-# exclusions=['LEIS', 'AOTH', 'HSOC', 'HOME', 'MANU', 'HOSP', 'BUSC' ,'WELS', 'WELF', 'IRIS', 'PERF', 'ECON', 'ENGI', 'OSCI'] discontinued/low entry
-# years=[2018,2019]
-#
-# for level in levels:
-# 	if level['name']==target:
-# 		os.chdir(os.path.dirname( __file__ ))
-# 		os.chdir('..')
-# 		os.chdir(level['output'])
-# 		with open(level['name'].lower()+'-subjects.json') as subjects_file:
-# 			subjects_data = json.load(subjects_file)
-# 		with open(level['name'].lower()+'-grades.json') as grades_file:
-# 			grades_data = json.load(grades_file)
-# 		output_dict = [x for x in grades_data if x['gender'] == gender and x['scope']==scope and x['name']==grade and x['alias'] not in exclusions]
-# 		for item in output_dict:
-# 			for subject in subjects_data:
-# 				if item['alias']==subject['alias']:
-# 					string=''
-# 					for year in years:
-# 						if item['data'][0][0]>year:		# add a blank value where year doesn't exist
-# 							string=string + ' '
-# 						for datum in item['data']:
-# 							if datum[0]==year:
-# 								if string=='':
-# 									string=str(datum[1])
-# 								else:
-# 									string=string + ', ' + str(datum[1])
-# 					try:
-# 						if string.rsplit(' ',1)[1]!='0.0':		# ditch subjects with no grades (and hence no entries) in most recent year
-# 							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string)
-# 					except IndexError:
-# 						if string!='0.0':		# ditch subjects with no grades (and hence no entries) in most recent year
-# 							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string)
-#
-# # GCSE, change in entries and grades in EBacc subjects
-# target='GCSE'
-# scope='EN'
-# y0=2018
-# yn=2019
-#
-# for level in levels:
-# 	if level['name']==target:
-# 		os.chdir(os.path.dirname( __file__ ))
-# 		os.chdir('..')
-# 		os.chdir(level['output'])
-# 		row_list=[]
-# 		with open(level['name'].lower()+'-subjects.json') as subjects_file:
-# 			subjects_data = json.load(subjects_file)
-# 		with open(level['name'].lower()+'-entries.json') as entries_file:
-# 			entries_data = json.load(entries_file)
-# 		with open(level['name'].lower()+'-grades.json') as grades_file:
-# 			grades_data = json.load(grades_file)
-# 		for subject in subjects_data:
-# 			if subject['flags']['ebacc'] == True:
-# 				alias=subject['alias']
-# 				row=OrderedDict([])
-# 				row['alias']=alias
-# 				if 'Discontinued' in str(subject['reform_year']['EN']):
-# 					row['discontinued']='Discontinued'
-# 				else:
-# 					row['discontinued']='Not discontinued'
-# 				row['subject_name_clean']=re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean'])
-# 				row['entries_y0']=''
-# 				row['entries_yn']=''
-# 				row['A_y0']=''
-# 				row['C_y0']=''
-# 				row['A_yn']=''
-# 				row['C_yn']=''
-# 				data=[]
-# 				for entries in entries_data:
-# 					if entries['name']=='All students' and entries['alias']==alias and entries['scope']==scope:
-# 						data=entries['data']
-# 						for data_item in data:
-# 							if data_item[0]==y0:
-# 								row['entries_y0']=data_item[1]
-# 							if data_item[0]==yn:
-# 								row['entries_yn']=data_item[1]
-# 						break
-# 				data=[]
-# 				for grades in grades_data:
-# 					if (grades['name']=='7/A or above' or grades['name']=='4/C or above') and grades['scope']==scope and grades['gender']=='All students' and grades['alias']==alias:
-# 						data=grades['data']
-# 						for data_item in data:
-# 							if data_item[0]==y0:
-# 								if grades['name']=='7/A or above':
-# 									row['A_y0']=data_item[1]
-# 								elif grades['name']=='4/C or above':
-# 									row['C_y0']=data_item[1]
-# 							if data_item[0]==yn:
-# 								if grades['name']=='7/A or above':
-# 									row['A_yn']=data_item[1]
-# 								elif grades['name']=='4/C or above':
-# 									row['C_yn']=data_item[1]
-# 				if row['entries_y0']!='' and row['entries_y0']!=0 and row['entries_yn']!=0 and row['entries_yn']!='':
-# 					print(row['alias'] + ', ' + row['subject_name_clean'] + ', ' + row['discontinued'] + ', ' + str(row['entries_y0']) + ', ' + str(row['entries_yn']) + ', ' + str(round((row['entries_yn']-row['entries_y0'])*100.0/row['entries_y0'],1)) + '%, ' + str(row['A_y0']) + ', ' + str(row['A_yn']))
-# 				else:
-# 					print(row['alias'] + ', ' + row['subject_name_clean'] + ', ' + row['discontinued'] + ', ' + str(row['entries_y0']) + ', ' + str(row['entries_yn']) + ', ' + 'n/a' + ', ' + str(row['A_y0']) + ', ' + str(row['A_yn']))
-#
-# # GCSE, total change in entries and grades in EBacc/non-EBacc subjects
-# target='GCSE'
-# # criteria='All'
-# criteria='Not discontinued'
-# scope='EN'
-# y0=2018
-# yn=2019
-#
-# for level in levels:
-# 	if level['name']==target:
-# 		os.chdir(os.path.dirname( __file__ ))
-# 		os.chdir('..')
-# 		os.chdir(level['output'])
-# 		row_list=[]
-# 		with open(level['name'].lower()+'-subjects.json') as subjects_file:
-# 			subjects_data = json.load(subjects_file)
-# 		with open(level['name'].lower()+'-entries.json') as entries_file:
-# 			entries_data = json.load(entries_file)
-# 		with open(level['name'].lower()+'-grades.json') as grades_file:
-# 			grades_data = json.load(grades_file)
-# 		for ebacc_flag in [True, False, None]:
-# 			row=OrderedDict([])
-# 			row['entries_y0']=''
-# 			row['entries_yn']=''
-# 			if ebacc_flag==True:
-# 				row['subject_name_clean']='EBacc'
-# 			elif ebacc_flag==False:
-# 				row['subject_name_clean']='Non-EBacc'
-# 			else:
-# 				row['subject_name_clean']='Total'
-# 			for subject in subjects_data:
-# 				if criteria=='All' or ('Discontinued' not in str(subject['reform_year']['EN'])):
-# 					if subject['flags']['ebacc']==ebacc_flag:		# all subjects is recorded as null
-# 						alias=subject['alias']
-# 						data=[]
-# 						for entries in entries_data:
-# 							if entries['name']=='All students' and entries['alias']==alias and entries['scope']==scope:
-# 								data=entries['data']
-# 								for data_item in data:
-# 									if data_item[0]==y0:
-# 										if row['entries_y0']=='':
-# 											row['entries_y0']=data_item[1]
-# 										else:
-# 											row['entries_y0']=int(row['entries_y0'])+data_item[1]
-# 									if data_item[0]==yn:
-# 										if row['entries_yn']=='':
-# 											row['entries_yn']=data_item[1]
-# 										else:
-# 											row['entries_yn']=int(row['entries_yn'])+data_item[1]
-# 								break
-# 						data=[]
-# 			print(row['subject_name_clean'] + ', ' + str(row['entries_y0']) + ', ' + str(row['entries_yn']) + ', ' + str(round((row['entries_yn']-row['entries_y0'])*100.0/row['entries_y0'],1)) + '%')
+# Listing discontinued subjects
+target='GCSE'
+for level in levels:
+	if level['name']==target:
+		os.chdir(os.path.dirname( __file__ ))
+		os.chdir('..')
+		os.chdir(level['output'])
+		row_list=[]
+		with open(level['name'].lower()+'-subjects.json') as subjects_file:
+			subjects_data = json.load(subjects_file)
+		for subject in subjects_data:
+			if 'Discontinued' in str(subject['reform_year']['EN']):
+				print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']), subject['reform_year']['EN'])
+
+# A-Level/AS-Level/GCSE: Extract entry figures for a given time range, and percentage change before y0 and yn
+target='A-Level'
+scope='EN'
+name='All students'
+years=[2018, 2019]
+
+for level in levels:
+	if level['name']==target:
+		os.chdir(os.path.dirname( __file__ ))
+		os.chdir('..')
+		os.chdir(level['output'])
+		with open(level['name'].lower()+'-subjects.json') as subjects_file:
+			subjects_data = json.load(subjects_file)
+		with open(level['name'].lower()+'-entries.json') as entries_file:
+			entries_data = json.load(entries_file)
+		output_dict = [x for x in entries_data if x['name'] == name and x['scope']==scope]
+		for item in output_dict:
+			for subject in subjects_data:
+				if item['alias']==subject['alias']:
+					string=''
+					entries_change=''
+					for year in years:
+						if item['data'][0][0]>year:		# add a blank value where year doesn't exist
+							string=string + ' '
+						for datum in item['data']:
+							if datum[0]==year:
+								if string=='':
+									entries_y0=datum[1]
+									string=str(datum[1])
+								else:
+									string=string + ', ' + str(datum[1])
+									try:
+										entries_change=round((datum[1]-entries_y0)/entries_y0*100.0,1)
+									except ZeroDivisionError:
+										entries_change=0
+					try:
+						if int(string.rsplit(' ',1)[1])>100 :		# ditch small-entry subjects
+							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string + ', ' + str(entries_change) + '%')
+					except IndexError:
+						if int(string)>100:		# ditch small-entry subjects
+							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string + ', ' + str(entries_change) + '%')
+
+# A-Level/AS-Level/GCSE: Extract grade figures for a given time range
+target='A-Level'
+scope='EN'
+gender='All students'
+grade='A or above'
+exclusions=[]
+# exclusions=['LEIS', 'AOTH', 'HSOC', 'HOME', 'MANU', 'HOSP', 'BUSC' ,'WELS', 'WELF', 'IRIS', 'PERF', 'ECON', 'ENGI', 'OSCI']		# discontinued/low entry
+years=[2018,2019]
+
+for level in levels:
+	if level['name']==target:
+		os.chdir(os.path.dirname( __file__ ))
+		os.chdir('..')
+		os.chdir(level['output'])
+		with open(level['name'].lower()+'-subjects.json') as subjects_file:
+			subjects_data = json.load(subjects_file)
+		with open(level['name'].lower()+'-grades.json') as grades_file:
+			grades_data = json.load(grades_file)
+		output_dict = [x for x in grades_data if x['gender'] == gender and x['scope']==scope and x['name']==grade and x['alias'] not in exclusions]
+		for item in output_dict:
+			for subject in subjects_data:
+				# if subject['flags']['ebacc'] == True:
+				if item['alias']==subject['alias']:
+					string=''
+					for year in years:
+						if item['data'][0][0]>year:		# add a blank value where year doesn't exist
+							string=string + ' '
+						for datum in item['data']:
+							if datum[0]==year:
+								if string=='':
+									string=str(datum[1])
+								else:
+									string=string + ', ' + str(datum[1])
+					try:
+						if string.rsplit(' ',1)[1]!='0.0':		# ditch subjects with no grades (and hence no entries) in most recent year
+							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string)
+					except IndexError:
+						if string!='0.0':		# ditch subjects with no grades (and hence no entries) in most recent year
+							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string)
+
+# GCSE: Produce total change in entries in EBacc/non-EBacc subjects
+target='GCSE'
+# criteria='All'
+criteria='Not discontinued'
+scope='EN'
+y0=2018
+yn=2019
+
+for level in levels:
+	if level['name']==target:
+		os.chdir(os.path.dirname( __file__ ))
+		os.chdir('..')
+		os.chdir(level['output'])
+		row_list=[]
+		with open(level['name'].lower()+'-subjects.json') as subjects_file:
+			subjects_data = json.load(subjects_file)
+		with open(level['name'].lower()+'-entries.json') as entries_file:
+			entries_data = json.load(entries_file)
+		with open(level['name'].lower()+'-grades.json') as grades_file:
+			grades_data = json.load(grades_file)
+		for ebacc_flag in [True, False, None]:
+			row=OrderedDict([])
+			row['entries_y0']=''
+			row['entries_yn']=''
+			if ebacc_flag==True:
+				row['subject_name_clean']='EBacc'
+			elif ebacc_flag==False:
+				row['subject_name_clean']='Non-EBacc'
+			else:
+				row['subject_name_clean']='Total'
+			for subject in subjects_data:
+				if criteria=='All' or ('Discontinued' not in str(subject['reform_year']['EN'])):
+					if subject['flags']['ebacc']==ebacc_flag:		# all subjects is recorded as null
+						alias=subject['alias']
+						data=[]
+						for entries in entries_data:
+							if entries['name']=='All students' and entries['alias']==alias and entries['scope']==scope:
+								data=entries['data']
+								for data_item in data:
+									if data_item[0]==y0:
+										if row['entries_y0']=='':
+											row['entries_y0']=data_item[1]
+										else:
+											row['entries_y0']=int(row['entries_y0'])+data_item[1]
+									if data_item[0]==yn:
+										if row['entries_yn']=='':
+											row['entries_yn']=data_item[1]
+										else:
+											row['entries_yn']=int(row['entries_yn'])+data_item[1]
+								break
+						data=[]
+			print(row['subject_name_clean'] + ', ' + str(row['entries_y0']) + ', ' + str(row['entries_yn']) + ', ' + str(round((row['entries_yn']-row['entries_y0'])*100.0/row['entries_y0'],1)) + '%')
 
 # A-Level/AS-Level/GCSE: Produce non-cumulative extract of grades data
 target = 'A-Level'
