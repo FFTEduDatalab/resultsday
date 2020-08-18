43f4fd89a9b448fd9cf037e5b57886784b92fb38
philipnye <contact@philipnye.co.uk>
2020-08-18 11:41:17 +0100

    Update for 2020 and minor tweaks

--- py/extract_generator.py
+++ py/extract_generator.py
@@ -32,6 +32,7 @@ levels=[
 nations=['UK','EN','WA','NI']
 ages=['15','16','17']
 
+
 # Listing EBacc subjects
 target='GCSE'
 for level in levels:
@@ -45,6 +46,7 @@ for level in levels:
 		for subject in subjects_data:
 			if subject['flags']['ebacc']==True:
 				print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']))		# strip out unicode characters - e.g. any zero-width space characters)
+
 
 # Listing discontinued subjects
 target='GCSE'
@@ -60,11 +62,12 @@ for level in levels:
 			if 'Discontinued' in str(subject['reform_year']['EN']):
 				print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']), subject['reform_year']['EN'])
 
-# A-Level/AS-Level/GCSE: Extract entry figures for a given time range, and percentage change before y0 and yn
+
+# A-Level/AS-Level/GCSE: Extract entry figures for a given time range, and percentage change between y0 and yn
 target='A-Level'
 scope='EN'
 name='All students'
-years=[2018, 2019]
+years=[2019, 2020]
 
 for level in levels:
 	if level['name']==target:
@@ -97,10 +100,11 @@ for level in levels:
 										entries_change=0
 					try:
 						if int(string.rsplit(' ',1)[1])>100 :		# ditch small-entry subjects
-							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string + ', ' + str(entries_change) + '%')
+							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string + ', ' + str(entries_change) + '%')		# strip out unicode characters - e.g. any zero-width space characters)
 					except IndexError:
 						if int(string)>100:		# ditch small-entry subjects
 							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string + ', ' + str(entries_change) + '%')
+
 
 # A-Level/AS-Level/GCSE: Extract grade figures for a given time range
 target='A-Level'
@@ -109,7 +113,7 @@ gender='All students'
 grade='A or above'
 exclusions=[]
 # exclusions=['LEIS', 'AOTH', 'HSOC', 'HOME', 'MANU', 'HOSP', 'BUSC' ,'WELS', 'WELF', 'IRIS', 'PERF', 'ECON', 'ENGI', 'OSCI']		# discontinued/low entry
-years=[2018,2019]
+years=[2019,2020]
 
 for level in levels:
 	if level['name']==target:
@@ -141,6 +145,58 @@ for level in levels:
 					except IndexError:
 						if string!='0.0':		# ditch subjects with no grades (and hence no entries) in most recent year
 							print(re.sub(r'[^\x00-\x7F]',' ', subject['subject_name_clean']) + ', ' + string)
+
+
+# A-Level/AS-Level/GCSE: Produce non-cumulative extract of grades data
+target = 'A-Level'
+scope = 'EN'
+gender = 'All students'
+exclusions = ['COMM','CRIT','GENS','ICTX','IRIS','PERF','WELF','WELS','AOTH']		# discontinued/low entry
+years = [2020, 2020]
+
+grade_dict = {
+	'subject': None,
+	'grade': None,
+	'pct_2020': None,
+	'pct_2020': None,
+}
+
+previous_pct_dict = {
+	2020: 0,
+	2020: 0,
+}
+
+grades_list = []
+
+for level in levels:
+	if level['name']==target:
+		os.chdir(os.path.dirname( __file__ ))
+		os.chdir('..')
+		os.chdir(level['output'])
+		with open(level['name'].lower()+'-subjects.json') as subjects_file:
+			subjects_data = json.load(subjects_file)
+		with open(level['name'].lower()+'-grades.json') as grades_file:
+			grades_data = json.load(grades_file)
+		output_dict = [x for x in grades_data if x['gender'] == gender and x['scope']==scope and x['alias'] not in exclusions]
+		for subject in subjects_data:
+			previous_pct_dict = dict.fromkeys(previous_pct_dict, 0)
+			for item in output_dict:
+				grade_dict_working = grade_dict.copy()
+				grade_dict_working['subject'] = subject['subject_name_clean']
+				if item['alias']==subject['alias']:
+					grade_dict_working['grade'] = re.sub(r' or above','', item['name'])		# as we're producing non-cumulative data
+					for year in years:
+						for datum in item['data']:
+							if datum[0]==year:
+								if year == 2020:
+									grade_dict_working['pct_2020'] = round((datum[1] - previous_pct_dict[year])/100.0, 3)		# producing non-cumulative data
+								if year == 2020:
+									grade_dict_working['pct_2020'] = round((datum[1] - previous_pct_dict[year])/100.0, 3)
+								previous_pct_dict[year] = datum[1]
+					grades_list.append(grade_dict_working)
+		with open('subjectgrade.json', 'w') as grades_file:		# prettified
+			json.dump(grades_list, grades_file, indent=4, separators=(',', ': '))
+
 
 # GCSE: Produce total change in entries in EBacc/non-EBacc subjects
 target='GCSE'
@@ -194,53 +250,3 @@ for level in levels:
 								break
 						data=[]
 			print(row['subject_name_clean'] + ', ' + str(row['entries_y0']) + ', ' + str(row['entries_yn']) + ', ' + str(round((row['entries_yn']-row['entries_y0'])*100.0/row['entries_y0'],1)) + '%')
-
-# A-Level/AS-Level/GCSE: Produce non-cumulative extract of grades data
-target = 'A-Level'
-scope = 'EN'
-gender = 'All students'
-exclusions = ['COMM','CRIT','GENS','ICTX','IRIS','PERF','WELF','WELS','AOTH']		# discontinued/low entry
-years = [2019, 2020]
-
-grade_dict = {
-	'subject': None,
-	'grade': None,
-	'pct_2019': None,
-	'pct_2020': None,
-}
-
-previous_pct_dict = {
-	2019: 0,
-	2020: 0,
-}
-
-grades_list = []
-
-for level in levels:
-	if level['name']==target:
-		os.chdir(os.path.dirname( __file__ ))
-		os.chdir('..')
-		os.chdir(level['output'])
-		with open(level['name'].lower()+'-subjects.json') as subjects_file:
-			subjects_data = json.load(subjects_file)
-		with open(level['name'].lower()+'-grades.json') as grades_file:
-			grades_data = json.load(grades_file)
-		output_dict = [x for x in grades_data if x['gender'] == gender and x['scope']==scope and x['alias'] not in exclusions]
-		for subject in subjects_data:
-			previous_pct_dict = dict.fromkeys(previous_pct_dict, 0)
-			for item in output_dict:
-				grade_dict_working = grade_dict.copy()
-				grade_dict_working['subject'] = subject['subject_name_clean']
-				if item['alias']==subject['alias']:
-					grade_dict_working['grade'] = re.sub(r' or above','', item['name'])		# as we're producing non-cumulative data
-					for year in years:
-						for datum in item['data']:
-							if datum[0]==year:
-								if year == 2019:
-									grade_dict_working['pct_2019'] = round((datum[1] - previous_pct_dict[year])/100.0, 3)		# producing non-cumulative data
-								if year == 2020:
-									grade_dict_working['pct_2020'] = round((datum[1] - previous_pct_dict[year])/100.0, 3)
-								previous_pct_dict[year] = datum[1]
-					grades_list.append(grade_dict_working)
-		with open('subjectgrade.json', 'w') as grades_file:		# prettified
-			json.dump(grades_list, grades_file, indent=4, separators=(',', ': '))
