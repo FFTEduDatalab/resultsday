fbb944db3401273d462ad1132600a8fc6aab27c5
dtbibby <77286195+dtbibby@users.noreply.github.com>
2024-08-22 09:59:47 +0100

    remove empty comment section, bump version

--- gcse/religious-studies.php
+++ gcse/religious-studies.php
@@ -3,7 +3,7 @@
 	<div class="container">
 		<div class="row" id="header-section">
 			<div class="col s12">
-				<a class="btn-flat" id="back-button" onclick="goBack()">< Back to subject list</a>
+				<a class="btn-flat" id="back-button" onclick="goBack()">&lt; Back to subject list</a>
 				<h1><span id='subjectNameContainer'></span> | <span id='levelNameContainer'></span></h1>
 			</div>
 		</div>
@@ -51,12 +51,12 @@
 			<div class="col l4 s12">
 				<ul class="collection">
 					<li class="collection-item" id='gcseFlagContainer'>
-						<div align='right'><a class="material-icons" data-position="top" href='/guide.php?v=20200923#subject_flags'>info_outline</a></div>
+						<div align='right'><a class="material-icons" data-position="top" href='/guide.php?v=20240815#subject_flags'>info_outline</a></div>
 						<img id='ebaccFlagImg' src='/img/ebaccFlagImgGrey.png' class='tooltipped' data-position="top" data-tooltip='This subject does not count in the English Baccalaureate (England only)'>
 						<img id='p8dblFlagImg' src='/img/p8dblFlagImgGrey.png' class='tooltipped' data-position="top" data-tooltip='This subject is not double-counted in Progress 8 calculations (England only)'>
 					</li>
 					<li class="collection-item">
-						<div align='right'><a class="material-icons" data-position="top" href='/guide.php?v=20200923#reform_date'>info_outline</a></div>
+						<div align='right'><a class="material-icons" data-position="top" href='/guide.php?v=20240815#reform_date'>info_outline</a></div>
 						<div id='reformYearContainer'></div>
 					</li>
 				</ul>
@@ -71,14 +71,14 @@
 					</li>
 				</ul>
 			</div>
-			<div class="col l4 s12 right">
+			<!--<div class="col l4 s12 right">
 				<ul class="collection">
 					<li class="collection-item">
 						<p>Share what you've learnt:</p>
 						<div class="addthis_inline_share_toolbox"></div>
 					</li>
 				</ul>
-			</div>
+			</div>-->
 		</div>
 		<div class="row" id="related-subjects-section">
 			<div class="col s12">
@@ -91,6 +91,6 @@
 		</div>
 	</div>
 </div>
-<script src='/js/results-subject.js?v=20200923'></script>
-<script src='/js/toasts.js?v=20200923'></script>
+<script src='/js/results-subject.js?v=20240815'></script>
+<script src='/js/toasts.js?v=20240815'></script>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php');?>
